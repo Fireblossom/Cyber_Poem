58eb6c5f8d5229bbc59e70afb67ff9b0ea41d8de
Sean Wong <titus.wang@outlook.com>
2020-01-26 14:51:57 +0100

    nn
    jjjj

--- encoding_tei.py
+++ encoding_tei.py
@@ -1,23 +1,97 @@
-from verse_encoding import preprocess
+# encoding=utf-8
+import json
+import re
 from xml.etree import ElementTree as ET
 from xml.dom import minidom
 
+def flatten(l):
+    return [item for sublist in l for item in sublist]
+
+def load_data():
+    with open("data/rhymebooks.json") as output:
+        rhymes = json.load(output)
+    with open("data/TC2SC.json") as output:
+        T2C = json.load(output)
+    with open("data/kangxi.json") as output:
+        Dict = json.load(output)
+    with open("data/metric.name") as output:
+        metric_names = [line.strip() for line in output]
+
+    P_PING_unflattened = rhymes['平水韵'][0]
+    Z_PING_unflattened = rhymes['平水韵'][1]
+    P_PING = flatten(rhymes['平水韵'][0])
+    Z_PING = flatten(rhymes['平水韵'][1])
+
+    return rhymes, T2C, Dict, metric_names, P_PING_unflattened, Z_PING_unflattened, P_PING, Z_PING
+
+def preprocess(raw):
+    _, _, _, _, _, _, P_PING, Z_PING = load_data()
+    clean = re.sub(r'[，。, .]', "", raw)
+    tokens = [token for token in clean if token]
+    tones  = []
+    for t in tokens:
+        if t in P_PING:
+            if t in Z_PING:
+                tones.append('1/0')
+            else:
+                tones.append('1')
+        else:
+            tones.append('0')
+    return tokens,tones
+
+
+def metricsAnalytics(verse, metrics):
+    _, _, _, metric_names, P_PING_unflattened, _, _, _ = load_data()
+    tokens, tones = verse
+    print('五绝')
+    count = 0
+    RBOOK = P_PING_unflattened
+    if tones[1] == '1':
+        print('平起')
+        metric = metrics[:2]
+    else:
+        print('仄起')
+        metric = metrics[2:]
+
+    for k, v in enumerate(RBOOK):
+        if tokens[9] in v and tokens[19] in v:
+            print("韵脚：", metric_names[k])
+            if tones[4] == '1':
+                num_metrics = 0
+                if tokens[4] in v:
+                    print('首句入韵')
+            else:
+                num_metrics = 1
+                print('首句仄收，不入韵, 首句白脚')
+
+                for k, (l1, l2) in enumerate(zip(tones, metric[num_metrics])):
+                    if l1 == '1/0':
+                        print("[检测到多音字", tokens[k], "]")
+                    elif l2 == '1/0':
+                        pass
+                    elif l1 != l2 and k % 5 != 0:
+                        if l1 == '0':
+                            print(tokens[k], "失配,建议：仄")
+                        else:
+                            print(tokens[k], "失配,建议：平")
+                        count += 1
+
+    print('失配词数', count)
 
 def xml_write(root, filepath):
     rough_string = ET.tostring(root, 'utf-8')
     reparsed = minidom.parseString(rough_string)
     raw_str = reparsed.toprettyxml(indent='', newl="")
+    # print(raw_str)
     file = open(filepath, 'w', encoding='utf-8')
     file.write('<?xml version="1.0" encoding="UTF-8"?>')
     file.write(raw_str)
     file.close()
 
 
-def build_tei_object(raw):
-    verse = preprocess(raw)
-    # metrics_analytics(verse, METRICS)
-    met = []
-
+def build_tei_object():
+    # verse = preprocess(raw)
+    # metricsAnalytics(verse, metrics)
 
     # Following https://teibyexample.org/examples/TBED04v00.htm
     TEI = ET.Element("TEI")
@@ -58,11 +132,16 @@ def build_tei_object(raw):
     top_lg = ET.SubElement(body, "lg")
     top_lg.attrib = {
         "type": "poem",
-        "met": met,
+        "met": "met",
     }
 
     head = ET.SubElement(top_lg, "head")
     title = ET.SubElement(head, "title")
     # title.text = # 填诗名和作者进去
+    # tree = ET.ElementTree(TEI)
+    # tree.write('output.xml' )
+    xml_write(TEI,'output.xml')
+    dom = minidom.parse('output.xml')  # or xml.dom.minidom.parseString(xml_string)
+    print(dom.toprettyxml())
 
     return TEI
